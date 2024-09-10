7364ddfa8ed96cd7dba1bc025a36ab76ae0d2c02
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-09-10 13:20:57 +0200

    början

---
+++ quiz/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine ("Kollar du på fotboll?\n a)Ja \n b)Nej \n c)Lite");
+string tall = Console.ReadLine();
+
+tall = tall.ToLower();
+
+// string name = "Renat";
+
+// bool isAlive = name == "Renat";
+
+// int hp = 66 + 33;
+
+// hp = hp + 1;
+
+// Console.WriteLine($"Du har {hp} hp kvar");
+
+// if (hp > 0)
+// {
+//     Console.WriteLine("yay! Du lever!");
+// }
+
+Console.ReadLine();
