37fa74b321f85b84fddb6a83a36546ad0e8c1b62
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-09-11 15:05:00 +0200

    ghfhg

--- quiz/Program.cs
+++ quiz/Program.cs
@@ -1,8 +1,40 @@
-﻿Console.WriteLine ("Kollar du på fotboll?\n a)Ja \n b)Nej \n c)Lite");
-string tall = Console.ReadLine();
+﻿fråga();
 
+static void fråga(){
+
+Console.WriteLine ("Kollar du på fotboll?\n a)Ja \n b)Nej \n c)Lite");
+string tall = Console.ReadLine();
 tall = tall.ToLower();
 
+
+if (tall == "a")
+{
+    Console.WriteLine("Va kul då.\n Vem är den bästa fotbollspelaren i värden \n a)Messi \n b)Ronaldo \n c)Neymar");
+
+    tall = Console.ReadLine();
+    tall = tall.ToLower();
+
+     if (tall == "a")
+    {
+        Console.WriteLine("Du bajs skaffa liv");
+    }
+    else if (tall == "b")
+        Console.WriteLine("Vilken legend due, respekt!");
+    
+    else if (tall == "c")
+        Console.WriteLine("Det är bra men tyvärr inte rätt svar.");
+    else
+    {
+        Console.WriteLine("Är du dum? Svara korrekt.");
+    }
+}
+else
+    {
+
+        Console.WriteLine("Är du dum? Svara korrekt.");
+        Console.ReadLine(); fråga();
+    }
+}
 // string name = "Renat";
 
 // bool isAlive = name == "Renat";
@@ -17,5 +49,3 @@ tall = tall.ToLower();
 // {
 //     Console.WriteLine("yay! Du lever!");
 // }
-
-Console.ReadLine();
